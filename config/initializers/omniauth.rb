34307d827d1ecd897c7abb2c59047e819803f941
Randy <randyisbusy@gmail.com>
2020-01-18 17:26:25 -0700

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-
-  if Rails.env.production? 
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']  
-  end
+  
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']  unless Rails.env.production?
+  
 end
